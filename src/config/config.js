53bf197f576dfab2a274c3fe84b9a0c85833ddc5
Rolando Guerrero <rolando22_@outlook.com>
2022-10-11 15:33:56 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const config = {
-       serverRoute: "https://siscomited-mern-sena.herokuapp.com/"
+       serverRoute: "https://backend-sena-production.up.railway.app/"
     // serverRoute: "http://localhost:4000/"
     //serverRoute: "http://localhost:4500/"
 }
